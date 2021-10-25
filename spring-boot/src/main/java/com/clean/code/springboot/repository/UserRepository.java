fc2bcbbb94682ae8bf9c6d6b0109271d5b3a6612
SolihaUZB <92523803+SolihaUZB@users.noreply.github.com>
2021-10-25 18:25:51 +0500

    http basic authontication create role and user entity

--- spring-boot/src/main/java/com/clean/code/springboot/repository/UserRepository.java
+++ spring-boot/src/main/java/com/clean/code/springboot/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.clean.code.springboot.repository;
 
+import com.clean.code.springboot.domain.Role;
 import com.clean.code.springboot.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
